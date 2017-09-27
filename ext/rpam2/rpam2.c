cc26c52c74b676afe410896fee1af3c7b5331c59
alex <alexander.carsten.kaftan@student.uni-augsburg.de>
2017-09-27 13:25:35 +0200

    fix authentication, add account check

--- ext/rpam2/rpam2.c
+++ ext/rpam2/rpam2.c
@@ -1,11 +1,57 @@
 #include "ruby.h"
 #include <security/pam_appl.h>
 
-static const char
-*rpam_default_servicename = "rpam";
+static const char *const
+rpam_default_servicename = "rpam";
+
+
+static VALUE
+method_authpam(VALUE self, VALUE servicename, VALUE username, VALUE password);
+
+static VALUE
+method_accountpam(VALUE self, VALUE servicename, VALUE username);
 
 VALUE rpam2;
-void Init_Rpam2();
+void Init_rpam2(){
+    rpam2 = rb_define_module("Rpam2");
+    rb_define_singleton_method(rpam2, "authpam", method_authpam, 3);
+    rb_define_singleton_method(rpam2, "accountpam", method_accountpam, 2);
+}
+
+int rpam_auth_conversation(int num_msg, const struct pam_message **msgm,
+                           struct pam_response **responses, void *appdata_ptr){
+    responses = calloc(num_msg, sizeof(struct pam_response));
+    // no space for responses
+    if (!responses)
+        return PAM_BUF_ERR;
+    char *pw = (char *)appdata_ptr;
+    for (int msgc=0; msgc<num_msg; msgc++){
+        switch (msgm[msgc]->msg_style) {
+            case PAM_PROMPT_ECHO_OFF:
+                // Assume ECHO_OFF is password/secret input
+                responses[msgc]->resp = strdup(pw);
+            case PAM_PROMPT_ECHO_ON:
+            case PAM_TEXT_INFO:
+                // ignore, they should not occur but some verbose applications exist always
+                responses[msgc]->resp = strdup("");
+                break;
+            case PAM_ERROR_MSG:
+                // print error message
+                rb_warn("%s", msgm[msgc]->msg);
+                responses[msgc]->resp = strdup("");
+                break;
+            default:
+                free(responses);
+                return PAM_CONV_ERR;
+        }
+        // response could not be allocated (no space)
+        if(responses[msgc]->resp==0){
+            free(responses);
+            return PAM_BUF_ERR;
+        }
+    }
+    return PAM_SUCCESS;
+}
 
 static VALUE method_authpam(VALUE self, VALUE servicename, VALUE username, VALUE password) {
     pam_handle_t* pamh = NULL;
@@ -17,14 +63,14 @@ static VALUE method_authpam(VALUE self, VALUE servicename, VALUE username, VALUE
     if(!NIL_P(servicename)){
         service = StringValueCStr(servicename);
     }
-    pam_start(service, StringValueCStr(username), NULL, &pamh);
+
+    struct pam_conv auth_c;
+    auth_c.conv = rpam_auth_conversation;
+    auth_c.appdata_ptr = StringValueCStr(password);
+
+    pam_start(service, StringValueCStr(username), &auth_c, &pamh);
     if (result != PAM_SUCCESS) {
-        return Qfalse;
-    }
-    // don't implement whole dialog
-    result = pam_set_item(pamh, PAM_AUTHTOK, StringValueCStr(password));
-    if (result != PAM_SUCCESS) {
-        pam_end(pamh, result);
+        rb_warn("pam initialisation failed");
         return Qfalse;
     }
 
@@ -42,11 +88,39 @@ static VALUE method_authpam(VALUE self, VALUE servicename, VALUE username, VALUE
 
     if (pam_end(pamh, result) == PAM_SUCCESS)
         return Qtrue;
-    else
+    else {
+        rb_warn("pam end failed");
         return Qfalse;
+    }
 }
 
-void Init_rpam2() {
-    rpam2 = rb_define_module("Rpam2");
-    rb_define_singleton_method(rpam2, "authpam", method_authpam, 3);
+static VALUE method_accountpam(VALUE self, VALUE servicename, VALUE username) {
+    pam_handle_t* pamh = NULL;
+    unsigned int result=0;
+    Check_Type(username, T_STRING);
+
+    char *service = rpam_default_servicename;
+    if(!NIL_P(servicename)){
+        service = StringValueCStr(servicename);
+    }
+
+    struct pam_conv auth_c = {0,0};
+    pam_start(service, StringValueCStr(username), &auth_c, &pamh);
+    if (result != PAM_SUCCESS) {
+        rb_warn("pam initialisation failed");
+        return Qfalse;
+    }
+
+    result = pam_acct_mgmt(pamh, 0);
+    if (result != PAM_SUCCESS) {
+        pam_end(pamh, result);
+        return Qfalse;
+    }
+
+    if (pam_end(pamh, result) == PAM_SUCCESS)
+        return Qtrue;
+    else {
+        rb_warn("pam end failed");
+        return Qfalse;
+    }
 }
--- rpam2.gemspec
+++ rpam2.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = "rpam2"
   s.version = "2.0.0"
-  s.date = "2017-09-26"
+  s.date = "2017-09-27"
   s.summary = "PAM integration with ruby."
   s.email = "devkral@web.de"
   s.description = "Ruby PAM (Pluggable Authentication
   Modules) integration"
   s.extra_rdoc_files = ["README.rdoc"]
-  s.authors = ["Alexander K."]
+  s.authors = ["Alexander Kaftan"]
   s.files = ["lib/rpam2.rb", "ext/rpam2/rpam2.c", "ext/rpam2/extconf.rb", "rpam2.gemspec", "README.rdoc", "LICENSE.txt"]
   s.has_rdoc = true
   s.license = "MIT"
