1ead88eb4ed17c34ca47b156d663cceec8a8f5d5
alex <devkral@web.de>
2018-03-09 09:35:37 +0100

    move function checks to non polyfilled build

--- ext/rpam2/extconf.rb
+++ ext/rpam2/extconf.rb
@@ -5,19 +5,17 @@ abort "missing pam library" unless have_library("pam","pam_start")
 use_pam_header = have_header("security/pam_appl.h")
 #use_pam_header = false
 
-
-
-have_func("pam_end")
-have_func("pam_open_session")
-have_func("pam_close_session")
-have_func("pam_authenticate")
-have_func("pam_acct_mgmt")
-have_func("pam_set_item")
-have_func("pam_get_item")
 $CFLAGS << " -std=c99 "
 
 if use_pam_header
   puts "Rpam2 build with pam header."
+  have_func("pam_end")
+  have_func("pam_open_session")
+  have_func("pam_close_session")
+  have_func("pam_authenticate")
+  have_func("pam_acct_mgmt")
+  have_func("pam_set_item")
+  have_func("pam_get_item")
 else
   puts "Rpam2 build without pam header, use pam polyfills."
   $CFLAGS << "-DWITHOUT_PAM_HEADER=1 "
